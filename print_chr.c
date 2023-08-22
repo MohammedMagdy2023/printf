28c4de345325ce882e638cb3b80d677b00a277f5
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-23 02:07:56 +0300

    C-Printf

--- print_chr.c
+++ print_chr.c
@@ -9,10 +9,10 @@
  */
 int print_chr(va_list arguments, char *buf, unsigned int ibuf)
 {
-	char c;
+char c;
 
-	c = va_arg(arguments, int);
-	handl_buf(buf, c, ibuf);
+c = va_arg(arguments, int);
+handl_buf(buf, c, ibuf);
 
-	return (1);
+return (1);
 }
